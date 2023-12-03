25a1598924f9ba489b324da0c48400773b47f3c1
brigerodev <62488289+lolhol@users.noreply.github.com>
2023-12-02 19:14:36 -0800

    chat gpt fix err

--- src/pages/gen.tsx
+++ src/pages/gen.tsx
@@ -1,113 +1,121 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import DropdownMenu from "../components/DropdownMenu";
-
-import css from "../styles/genStyle.module.css";
 import Test from "./test";
-
-let prompt: string;
-let results: string[];
-
-let isDone = false;
-let c = 0;
 
 export default function Gen() {
   const [isGenerated, setGen] = useState(false);
   const [lessonName, setLessonName] = useState("NONE");
   const [chapter, setChapter] = useState("NONE");
-  
   const [question, setQ] = useState("NONE");
   const [answers, setA] = useState<string[]>([]);
+  const [isDone, setIsDone] = useState(false); // Manage isDone as a state
 
   const handleExecute = async (lessonName: string, chapter: string) => {
-    if (c >= 1) {
-      return;
-    }
+    console.log(lessonName);
+    console.log(chapter);
 
-    c++
-  
-    console.log(lessonName)
-    console.log(chapter)
-  
-    let a = JSON.stringify({
-      textbook: lessonName,
-      instructions: "You are a multiple choice test creator.",
-      prompt: "Provide a challenging question from chapter " + chapter + " of The Book Theif, do not provide any anwers just simply the question. Only provide the raw question with nothing else."
-    });
-  
-    const question = await fetch("./api/getRes", {
-      method: "POST",
-      body: a
-    })
-  
-  
-    const jsonQuestion = await question.json();
-    console.log(jsonQuestion.val);
-  
-    let b = JSON.stringify({
-      textbook: lessonName,
-      instructions: "You are a multiple choice test creator.",
-      prompt: "There are 4 people providing answers to the following question in the context of a multiple choise quiz:" + jsonQuestion.val +" You are responsible for providing the correct answer to this question. Only provide the raw answer with nothing else.",
-    })
-  
-    const correctanswer = await fetch("./api/getRes", {
-      method: "POST",
-      body: b
-    })
-  
-    let answerchoices: string[] = [];
-    const jsonCorrectAnswer = await correctanswer.json();
-    answerchoices.push(jsonCorrectAnswer.val);
-    
-    console.log(jsonCorrectAnswer.val);
-  
-    for (let i = 0; i < 3; i++){
-      let c = JSON.stringify({
+    try {
+      const a = JSON.stringify({
         textbook: lessonName,
-        instructions: "You are a multiple choice test creator.",
-        prompt: "There are 4 people providing answers to the following question in the context of a multiple choise quiz:" + jsonQuestion.val +" You are responsible for providing one of the incorrect answers to this question. Only provide the raw answer with nothing else.",
-      })
-  
-      const Incorrectanswer = await fetch("./api/getRes", {
+        instructions:
+          "You are a multiple choice test creator.",
+        prompt:
+          "Provide a challenging question from chapter " +
+          chapter +
+          " of The Book Theif, do not provide any anwers just simply the question. Only provide the raw question with nothing else.",
+      });
+
+      const questionResponse = await fetch("./api/getRes", {
         method: "POST",
-        body: c
-      })
-  
-      const jsonIncorrectAnswer = await Incorrectanswer.json();
-      answerchoices.push(jsonIncorrectAnswer.val);
-      console.log(jsonIncorrectAnswer.val);
+        body: a,
+      });
+
+      const jsonQuestion = await questionResponse.json();
+      console.log(jsonQuestion.val);
+
+      const b = JSON.stringify({
+        textbook: lessonName,
+        instructions:
+          "You are a multiple choice test creator.",
+        prompt:
+          "There are 4 people providing answers to the following question in the context of a multiple choise quiz:" +
+          jsonQuestion.val +
+          " You are responsible for providing the correct answer to this question. Only provide the raw answer with nothing else.",
+      });
+
+      const correctAnswerResponse = await fetch("./api/getRes", {
+        method: "POST",
+        body: b,
+      });
+
+      let answerchoices: string[] = [];
+      const jsonCorrectAnswer = await correctAnswerResponse.json();
+      answerchoices.push(jsonCorrectAnswer.val);
+
+      console.log(jsonCorrectAnswer.val);
+
+      for (let i = 0; i < 3; i++) {
+        const c = JSON.stringify({
+          textbook: lessonName,
+          instructions:
+            "You are a multiple choice test creator.",
+          prompt:
+            "There are 4 people providing answers to the following question in the context of a multiple choise quiz:" +
+            jsonQuestion.val +
+            " You are responsible for providing one of the incorrect answers to this question. Only provide the raw answer with nothing else.",
+        });
+
+        const incorrectAnswerResponse = await fetch("./api/getRes", {
+          method: "POST",
+          body: c,
+        });
+
+        const jsonIncorrectAnswer = await incorrectAnswerResponse.json();
+        answerchoices.push(jsonIncorrectAnswer.val);
+        console.log(jsonIncorrectAnswer.val);
+      }
+
+      setQ(jsonQuestion.val);
+      setA(answerchoices);
+      setIsDone(true); // Set isDone to true when everything is completed
+    } catch (error) {
+      console.error("Error:", error);
     }
-    
-    setQ(jsonQuestion.val);
-    setA(answerchoices);
   };
+
+  useEffect(() => {
+    if (isGenerated) {
+      handleExecute(lessonName, chapter);
+    }
+  }, [isGenerated]); // Trigger handleExecute when isGenerated changes
 
   return (
     <main>
       {!isGenerated && (
-        <>
-          <div>
-              <DropdownMenu propsClass={function (buttonName: string): void {
-                setLessonName(buttonName);
-              } } propsLesson={function (buttonName: string): void {
-                setChapter(buttonName);
-              } } />
-
-              <button onClick={() => { 
-                setGen(true);
-                handleExecute(lessonName, chapter); 
-              } }>GENERATE</button>
-          </div>
-        </>
+        <div>
+          <DropdownMenu
+            propsClass={(buttonName: string) => {
+              setLessonName(buttonName);
+            }}
+            propsLesson={(buttonName: string) => {
+              setChapter(buttonName);
+            }}
+          />
+          <button onClick={() => setGen(true)}>GENERATE</button>
+        </div>
       )}
 
-      {isGenerated && isDone && (
-        <>
-          <div>
-            <Test p={prompt} a={results} callback={function (): void {
+      {isDone && (
+        <div>
+          <Test
+            p={question}
+            a={answers}
+            callback={() => {
               setGen(false);
-            } } />
-          </div>
-        </>
+              setIsDone(false);
+            }}
+          />
+        </div>
       )}
     </main>
   );
