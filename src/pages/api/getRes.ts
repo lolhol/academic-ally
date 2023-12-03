4547d2c55267403c808494f49a7f621115b2cf10
Tony <tonywu3758@gmail.com>
2023-12-02 17:37:20 -0800

    A bunch of AI Stuff
    Basically everything is done

--- src/pages/api/getRes.ts
+++ src/pages/api/getRes.ts
@@ -1,11 +1,12 @@
-import * as fs from "fs";
+import * as fs from 'fs'
+
 import { NextApiRequest, NextApiResponse } from "next";
 import OpenAI from "openai"
 
 export default async function handler( req: NextApiRequest, res: NextApiResponse) {
     const parsed = JSON.parse(req.body);
     const openai = new OpenAI({
-        apiKey: "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"
+        apiKey: "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"
     });
     let ret = "---";
     if (parsed.textbook == "chemistry"){
@@ -69,5 +70,35 @@ export default async function handler( req: NextApiRequest, res: NextApiResponse
             }
         }
     }
-    res.status(200).json({sucess: true, val: ret});
+    if (parsed.textbook == "Book Thief"){
+        const assistant = await openai.beta.assistants.create({
+            name: "Book Thief Tutor",
+            instructions: parsed.instructions,
+            tools: [{ type: "code_interpreter" }],
+            model: "gpt-4-1106-preview",
+        });
+        const thread = await openai.beta.threads.create();
+        const message = await openai.beta.threads.messages.create(
+            thread.id,
+            {role: "user", content: parsed.prompt}
+        )
+        const run = await openai.beta.threads.runs.create(
+            thread.id,
+            {assistant_id: assistant.id}
+        )
+        while (true){
+            const run_status = await openai.beta.threads.runs.retrieve(
+                thread.id,
+                run.id
+            )
+            if (run_status.status == "completed"){
+                const message = await openai.beta.threads.messages.list(
+                    thread.id
+                )
+                ret = message.data[0].content[0].text.value;
+                break;
+            }
+        }
+    }
+    res.status(200).json({success: true, val: ret});
 }
