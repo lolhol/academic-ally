c0c1fd0fe9d3c1d63f2327ab5535526498b0e932
brigerodev <62488289+lolhol@users.noreply.github.com>
2023-12-02 19:05:43 -0800

    hopefully working

--- src/pages/gen.tsx
+++ src/pages/gen.tsx
@@ -12,10 +12,72 @@ let c = 0;
 
 export default function Gen() {
   const [isGenerated, setGen] = useState(false);
-
   const [lessonName, setLessonName] = useState("NONE");
-
   const [chapter, setChapter] = useState("NONE");
+  
+  const [question, setQ] = useState("NONE");
+  const [answers, setA] = useState<string[]>([]);
+
+  const handleExecute = async (lessonName: string, chapter: string) => {
+    if (c >= 1) {
+      return;
+    }
+  
+    console.log(lessonName)
+    console.log(chapter)
+  
+    let a = JSON.stringify({
+      textbook: lessonName,
+      instructions: "You are a multiple choice test creator.",
+      prompt: "Provide a challenging question from chapter " + chapter + " of The Book Theif, do not provide any anwers just simply the question. Only provide the raw question with nothing else."
+    });
+  
+    const question = await fetch("./api/getRes", {
+      method: "POST",
+      body: a
+    })
+  
+  
+    const jsonQuestion = await question.json();
+    console.log(jsonQuestion.val);
+  
+    let b = JSON.stringify({
+      textbook: lessonName,
+      instructions: "You are a multiple choice test creator.",
+      prompt: "There are 4 people providing answers to the following question in the context of a multiple choise quiz:" + jsonQuestion.val +" You are responsible for providing the correct answer to this question. Only provide the raw answer with nothing else.",
+    })
+  
+    const correctanswer = await fetch("./api/getRes", {
+      method: "POST",
+      body: b
+    })
+  
+    let answerchoices: string[] = [];
+    const jsonCorrectAnswer = await correctanswer.json();
+    answerchoices.push(jsonCorrectAnswer.val);
+    
+    console.log(jsonCorrectAnswer.val);
+  
+    for (let i = 0; i < 3; i++){
+      let c = JSON.stringify({
+        textbook: lessonName,
+        instructions: "You are a multiple choice test creator.",
+        prompt: "There are 4 people providing answers to the following question in the context of a multiple choise quiz:" + jsonQuestion.val +" You are responsible for providing one of the incorrect answers to this question. Only provide the raw answer with nothing else.",
+      })
+  
+      const Incorrectanswer = await fetch("./api/getRes", {
+        method: "POST",
+        body: c
+      })
+  
+      const jsonIncorrectAnswer = await Incorrectanswer.json();
+      answerchoices.push(jsonIncorrectAnswer.val);
+      console.log(jsonIncorrectAnswer.val);
+    }
+    
+    setQ(jsonQuestion.val);
+    setA(answerchoices);
+  };
 
   return (
     <main>
@@ -38,11 +100,6 @@ export default function Gen() {
 
       {isGenerated && isDone && (
         <>
-          <script>
-            console.log(
-              "!!!!"
-            )
-          </script>
           <div>
             <Test p={prompt} a={results} callback={function (): void {
               setGen(false);
@@ -53,65 +110,3 @@ export default function Gen() {
     </main>
   );
 }
-
-const handleExecute = async (lessonName: string, chapter: string) => {
-  if (c >= 1) {
-    return;
-  }
-
-  console.log(lessonName)
-  console.log(chapter)
-
-  let a = JSON.stringify({
-    textbook: lessonName,
-    instructions: "You are a multiple choice test creator.",
-    prompt: "Provide a challenging question from chapter " + chapter + " of The Book Theif, do not provide any anwers just simply the question. Only provide the raw question with nothing else."
-  });
-
-  const question = await fetch("./api/getRes", {
-    method: "POST",
-    body: a
-  })
-
-
-  const jsonQuestion = await question.json();
-  console.log(jsonQuestion.val);
-
-  let b = JSON.stringify({
-    textbook: lessonName,
-    instructions: "You are a multiple choice test creator.",
-    prompt: "There are 4 people providing answers to the following question in the context of a multiple choise quiz:" + jsonQuestion.val +" You are responsible for providing the correct answer to this question. Only provide the raw answer with nothing else.",
-  })
-
-  const correctanswer = await fetch("./api/getRes", {
-    method: "POST",
-    body: b
-  })
-
-  let answerchoices = [];
-  const jsonCorrectAnswer = await correctanswer.json();
-  answerchoices.push(jsonCorrectAnswer.val);
-  
-  console.log(jsonCorrectAnswer.val);
-
-  for (let i = 0; i < 3; i++){
-    let c = JSON.stringify({
-      textbook: lessonName,
-      instructions: "You are a multiple choice test creator.",
-      prompt: "There are 4 people providing answers to the following question in the context of a multiple choise quiz:" + jsonQuestion.val +" You are responsible for providing one of the incorrect answers to this question. Only provide the raw answer with nothing else.",
-    })
-
-    const Incorrectanswer = await fetch("./api/getRes", {
-      method: "POST",
-      body: c
-    })
-
-    const jsonIncorrectAnswer = await Incorrectanswer.json();
-    answerchoices.push(jsonIncorrectAnswer.val);
-    console.log(jsonIncorrectAnswer.val);
-  }
-  
-  results = answerchoices;
-  prompt = jsonQuestion.val;
-  isDone = true;
-};
