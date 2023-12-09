172b2803ac7a647b4d6619b6ca3346bdac1155a6
brigerodev <62488289+lolhol@users.noreply.github.com>
2023-12-08 16:44:45 -0800

    add textbook manager + add "on server startup" call of init

--- src/internal/AcademicAlly.ts
+++ src/internal/AcademicAlly.ts
@@ -1,13 +1,11 @@
 import GPTManager from "./GPTManager";
-import AssistentResponceStore from "./User/AssistentResponceStore";
 
-export const MANAGER: GPTManager = new GPTManager();
+export let MANAGER: GPTManager | undefined = undefined;
 
 // @This will have to be re-worked l8tr
-export const TEXTBOOKS = ["The Book Thief", "Geometry"];
-export const TEXTBOOKCHAPTERS = [
-  [1, 2, 3, 4, 5, 6],
-  [1, 2, 3, 4, 5, 6],
-];
-
+export const TEXTBOOKS = ["The-Book-Thief"];
 export const TOKENLEN = 10;
+
+export function register() {
+  MANAGER = new GPTManager();
+}
--- src/internal/GPTManager.ts
+++ src/internal/GPTManager.ts
@@ -1,21 +1,24 @@
-import { TEXTBOOKCHAPTERS, TEXTBOOKS } from "./AcademicAlly";
+import { TEXTBOOKS } from "./AcademicAlly";
 import Assistant from "./Assistant";
 import InvalidKeyException from "./err/InvalidKeyException";
 import AssistentResponce from "./User/AssistentResponce";
 import AssistentResponceStore from "./User/AssistentResponceStore";
 import * as fs from "fs";
+import TextbookManager from "./textbooks/TextbookManager";
 
 export default class GPTManager {
   private assistantMap: Map<string, Map<string, Assistant>> = new Map();
   private responceStore: AssistentResponceStore = new AssistentResponceStore();
-  private TEXTBOOKS: string[] = ["124", "1111"];
-  private TEXTBOOKCHAPTERS: string[] = ["1234"];
   private key: string;
+  private textBookManager: TextbookManager;
 
   constructor() {
     this.key = fs.readFileSync("./.env", "utf8");
+    this.textBookManager = new TextbookManager(TEXTBOOKS);
     this.createAssistants();
   }
+
+  private indexTextbooks() {}
 
   public addResponce(token: string, question: string, answers: string[]) {
     this.responceStore.set(token, question, answers);
@@ -27,20 +30,30 @@ export default class GPTManager {
 
   public createAssistants() {
     try {
-      for (let i = 0; i < this.TEXTBOOKS.length; i++) {
-        let textBookName = this.TEXTBOOKS[i];
-        let curChapterList = this.TEXTBOOKCHAPTERS[i];
+      for (let i = 0; i < TEXTBOOKS.length; i++) {
+        let textBookName = TEXTBOOKS[i];
+        let curChapterList = this.textBookManager.getBookChapterData(
+          textBookName,
+          i
+        );
+
+        if (curChapterList === undefined) continue;
 
         let curChapters: Map<string, Assistant> = new Map();
         for (let j = 0; j < curChapterList.length; j++) {
           let assistant = new Assistant(
             textBookName,
             this.key,
-            textBookName,
+            curChapterList[j],
             j.toString()
           );
 
-          curChapters.set(curChapterList[j].toString(), assistant);
+          // TODO: "multithreading" (l8tr)
+          /*for (let k = 1; k < 5; k++) {
+            curChapters.set(j.toString() + "_" + k.toString(), assistant);
+          }*/
+
+          curChapters.set(j.toString(), assistant);
         }
 
         this.assistantMap.set(textBookName, curChapters);
