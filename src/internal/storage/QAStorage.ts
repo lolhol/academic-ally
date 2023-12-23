db1ee72bdc875c8d8de4ed9401cf97747e3c0889
brigerodev <62488289+lolhol@users.noreply.github.com>
2023-12-23 12:43:44 -0800

    add rough add data framework to the qastorage

--- src/internal/storage/QAStorage.ts
+++ src/internal/storage/QAStorage.ts
@@ -1,11 +1,28 @@
 import * as fs from "fs";
 
-// TODO: make @this (its 2am rn not gon do today)
 export default class QAStorage {
   private fileLoc: string = "./public/qadata/data.json";
 
-  public addToFile(question: string, answers: string[]) {
-    let jsonData = fs.readFileSync(this.fileLoc, "utf-8");
-    let data = JSON.parse(jsonData);
+  public addToFile(
+    question: string,
+    answers: string[],
+    book: string,
+    chapter: number
+  ) {
+    const filePath = "./public/qadata/" + book + "_" + chapter + ".json";
+    if (!fs.existsSync(filePath)) {
+      fs.writeFileSync(filePath, "{'end': ['0']}", "utf-8");
+    }
+
+    const readData: Buffer = fs.readFileSync(filePath);
+    let json: { [key: string]: string[] } = JSON.parse(readData.toString());
+    const lastElemSpot = json["end"][1] + 1;
+    let arr = [];
+    arr.push(question);
+    arr.concat(answers);
+    json[lastElemSpot] = arr;
+
+    const stringVers = JSON.stringify(json);
+    fs.writeFileSync(filePath, stringVers);
   }
 }
