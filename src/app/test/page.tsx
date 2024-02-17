5c543e213d028df7177844cce4941a48b144ac2c
brigerodev <62488289+lolhol@users.noreply.github.com>
2024-02-16 16:25:02 -0800

    delete old stuff

---
+++ src/app/test/page.tsx
@@ -0,0 +1,10 @@
+import DropDownMenu from "../components/DropDownMenu";
+
+export default function Tests() {
+  return (
+    <main>
+      <div>This is a page for testing purposes</div>
+      <DropDownMenu options={[]} sizePx={0} />
+    </main>
+  );
+}
