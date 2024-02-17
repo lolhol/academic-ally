d3b9b70208488b63fe471a0f2abb9000f279a6bc
brigerodev <62488289+lolhol@users.noreply.github.com>
2024-02-16 18:41:12 -0800

    add dropdown menu

--- src/app/components/DropDownMenu.tsx
+++ src/app/components/DropDownMenu.tsx
@@ -1,13 +1,78 @@
+"use client";
+
+import { useState } from "react";
+import css from "./css/DropDownMenuCss.module.css";
+
 interface Option {
   onClick: (buttonName: string) => void;
-  name: string;
+  text: string;
 }
 
-interface Dropdown {
+interface OptionsCollection {
   options: Option[];
-  sizePx: number;
 }
 
-export default function DropDownMenu(options: Dropdown) {
-  return <div>eeeeeez</div>;
+export default function DropDownMenu(options: OptionsCollection) {
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const [isHoveringOverDiv, setHoveringOverDiv] = useState(false);
+
+  const handleOptionClick = (option: string) => {
+    // TODO: this will need to be re-coded
+    for (let i = 0; i < option.length; i++) {
+      if (options.options[i].text == option) {
+        options.options[i].onClick(option);
+        break;
+      }
+    }
+  };
+
+  return (
+    <div
+      className={css["dropdown-main"]}
+      onMouseEnter={() => setHoveringOverDiv(true)}
+      onMouseLeave={() => {
+        if (isDropdownOpen) {
+          setDropdownOpen(false);
+        }
+
+        setHoveringOverDiv(false);
+      }}
+    >
+      <button
+        className={css["dropdown-main-button"]}
+        onMouseEnter={() => setDropdownOpen(true)}
+      >
+        Select an option
+      </button>
+
+      <div
+        className={
+          !isDropdownOpen
+            ? css["dropdown-main-button-border-default"]
+            : css["dropdown-main-button-border-open"]
+        }
+      ></div>
+
+      {isDropdownOpen && (
+        <div
+          onMouseLeave={() => {
+            setDropdownOpen(false);
+          }}
+          className={css["dropdown-options-div"]}
+        >
+          {options.options.map((option) => (
+            <div>
+              <button
+                key={option.text}
+                className={css["dropdown-option-button"]}
+                onClick={() => handleOptionClick(option.text)}
+              >
+                {option.text}
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,10 +1,20 @@
+"use client";
+
 import DropDownMenu from "../components/DropDownMenu";
 
 export default function Tests() {
+  const handleOptionClick = (option: string) => {
+    console.log("!!!");
+  };
+
+  const options = [
+    { text: "Option 1", onClick: handleOptionClick },
+    { text: "Option 2", onClick: handleOptionClick },
+  ];
+
   return (
     <main>
-      <div>This is a page for testing purposes</div>
-      <DropDownMenu options={[]} sizePx={0} />
+      <DropDownMenu options={options} />
     </main>
   );
 }
